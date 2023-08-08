ec806ee0a25ed3b38d48709a16d7f9012984f4db
Adam Ramberg <adam@mambojambostudios.com>
2023-08-08 07:20:29 +0200

    * Setup routing

---
+++ Assets/Scenes/Docs/Docs.Routing.cs
@@ -0,0 +1,165 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Fiber;
+using Fiber.UIElements;
+using Fiber.Router;
+using Fiber.UI;
+using Signals;
+
+public static class DocsRouting
+{
+    public static class ROUTES
+    {
+        // Layout routes
+        public const string ROOT = "root";
+
+        // Path routes
+        public const string INTRODUCTION = "introduction";
+        public const string INSTALLATION = "installation";
+    }
+
+    public class RootComponent : BaseComponent
+    {
+        public override VirtualNode Render()
+        {
+            return new DocsThemes.Provider(children: F.Children(
+                F.UIDocument(
+                    name: "DocsRootDocument",
+                    children: F.Children(new MainLayoutComponent())
+                )
+            ));
+        }
+    }
+
+    public class MainLayoutComponent : BaseComponent
+    {
+        public override VirtualNode Render()
+        {
+            var theme = C<ThemeStore>().Get();
+            var neutral = "neutral";
+            var deepNeutral = "deepNeutral";
+
+            var router = C<Router>();
+            var selectedItemId = F.CreateComputedSignal((router) =>
+            {
+                var route = router.PeekRoute();
+                return route?.Id;
+            }, router);
+
+            return F.View(
+                style: new Style(
+                    display: DisplayStyle.Flex,
+                    height: new Length(100, LengthUnit.Percent),
+                    width: new Length(100, LengthUnit.Percent),
+                    alignItems: Align.Stretch,
+                    flexDirection: FlexDirection.Row
+                ),
+                children: F.Children(
+                    F.View(
+                        style: new Style(
+                            minWidth: 100,
+                            maxWidth: 240,
+                            width: new Length(25, LengthUnit.Percent),
+                            height: new Length(100, LengthUnit.Percent),
+                            backgroundColor: theme.DesignTokens[deepNeutral].Background.Default,
+                            borderRightColor: theme.DesignTokens[deepNeutral].Border.Default,
+                            borderRightWidth: 1,
+                            flexShrink: 0,
+                            flexGrow: 0
+                        ),
+                        children: F.Children(new TreeViewComponent.Container(
+                            role: deepNeutral,
+                            selectedItemId: selectedItemId,
+                            onItemIdSelected: (string id) =>
+                            {
+                                router.Navigate(id);
+                            },
+                            children: F.Children(
+                                new TreeViewComponent.Item(id: ROUTES.INTRODUCTION, label: $"Introduction"),
+                                new TreeViewComponent.Item(id: ROUTES.INSTALLATION, label: $"Installation")
+                            )
+                        ))
+                    ),
+                    F.View(
+                        style: new Style(
+                            backgroundColor: theme.DesignTokens[neutral].Background.Default,
+                            minHeight: new Length(100, LengthUnit.Percent),
+                            flexShrink: 1,
+                            flexGrow: 1,
+                            minWidth: new Length(100, LengthUnit.Pixel)
+                        ),
+                        children: F.Children(F.Outlet())
+                    )
+                )
+            );
+        }
+    }
+
+    public static RouteDefinition ROUTER_TREE = new RouteDefinition(
+        id: ROUTES.ROOT,
+        isLayoutRoute: true,
+        component: new SimpleRouteComponent(new RootComponent()),
+        children: new List<RouteDefinition>()
+        {
+            new RouteDefinition(
+                id: ROUTES.INTRODUCTION,
+                isLayoutRoute: false,
+                component: new SimpleRouteComponent(component: new IntroductionComponent())
+            ),
+            new RouteDefinition(
+                id: ROUTES.INSTALLATION,
+                isLayoutRoute: false,
+                component: new SimpleRouteComponent(component: new InstallationComponent())
+            ),
+        }
+    );
+
+    public class IntroductionComponent : BaseComponent
+    {
+        public override VirtualNode Render()
+        {
+            var theme = C<ThemeStore>().Get();
+
+            return F.Text(
+                text: "Introduction",
+                style: new Style(
+                    color: theme.DesignTokens["neutral"].Text.Default,
+                    fontSize: 36,
+                    paddingLeft: 18,
+                    paddingTop: 18,
+                    paddingRight: 18,
+                    paddingBottom: 18
+                )
+            );
+        }
+    }
+
+    public class InstallationComponent : BaseComponent
+    {
+        public override VirtualNode Render()
+        {
+            var theme = C<ThemeStore>().Get();
+
+            return F.Text(
+                text: "Installation",
+                style: new Style(
+                    color: theme.DesignTokens["neutral"].Text.Default,
+                    fontSize: 36,
+                    paddingLeft: 18,
+                    paddingTop: 18,
+                    paddingRight: 18,
+                    paddingBottom: 18
+                )
+            );
+        }
+    }
+
+    public class RouterProvider : BaseComponent
+    {
+        public override VirtualNode Render()
+        {
+            return F.RouterProvider(new Router(ROUTER_TREE).Navigate(ROUTES.INTRODUCTION));
+        }
+    }
+}
--- Assets/Scenes/Docs/DocsFiberRoot.cs
+++ Assets/Scenes/Docs/DocsFiberRoot.cs
@@ -1,10 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
-using System;
 using UnityEngine;
 using UnityEngine.UIElements;
 using Fiber;
-using Fiber.GameObjects;
 using Fiber.UIElements;
 using Fiber.Suite;
 using Signals;
@@ -16,99 +12,6 @@ public class DocsFiberRoot : MonoBehaviour
     private PanelSettings _panelSettings;
     private FiberSuite _fiber;
 
-    public class DocsAppComponent : BaseComponent
-    {
-        public override VirtualNode Render()
-        {
-            return new DocsThemes.Provider(children: F.Children(
-                F.UIDocument(
-                    name: "DocsDocument",
-                    children: F.Children(
-                        new DocsContentComponent()
-                    )
-                )
-            ));
-        }
-    }
-
-    public class DocsContentComponent : BaseComponent
-    {
-        public override VirtualNode Render()
-        {
-            var theme = C<ThemeStore>().Get();
-            var neutral = "neutral";
-            var deepNeutral = "deepNeutral";
-            var selectedItemId = new Signal<string>("1");
-
-            return F.View(
-                style: new Style(
-                    display: DisplayStyle.Flex,
-                    height: new Length(100, LengthUnit.Percent),
-                    width: new Length(100, LengthUnit.Percent),
-                    alignItems: Align.Stretch,
-                    flexDirection: FlexDirection.Row
-                ),
-                children: F.Children(
-                    F.View(
-                        style: new Style(
-                            minWidth: 100,
-                            maxWidth: 240,
-                            width: new Length(25, LengthUnit.Percent),
-                            height: new Length(100, LengthUnit.Percent),
-                            backgroundColor: theme.DesignTokens[deepNeutral].Background.Default,
-                            borderRightColor: theme.DesignTokens[deepNeutral].Border.Default,
-                            borderRightWidth: 1,
-                            flexShrink: 0,
-                            flexGrow: 0
-                        ),
-                        children: F.Children(new TreeViewComponent.Container(
-                            role: deepNeutral,
-                            selectedItemId: selectedItemId,
-                            onItemIdSelected: (string id) =>
-                            {
-                                selectedItemId.Value = id;
-                            },
-                            children: F.Children(
-                                new TreeViewComponent.Item(id: "1", label: $"Item 1"),
-                                new TreeViewComponent.Item(id: "2", label: $"Item 2", children: F.Children(
-                                    new TreeViewComponent.Item(id: "2.1", label: $"Item 2.1"),
-                                    new TreeViewComponent.Item(id: "2.2", label: $"Item 2.2", children: F.Children(
-                                        new TreeViewComponent.Item(id: "2.2.1", label: $"Item 2.2.1 Item 2.2.1 Item 2.2.1"),
-                                        new TreeViewComponent.Item(id: "2.2.2", label: $"Item 2.2.2")
-                                    )),
-                                    new TreeViewComponent.Item(id: "2.3", label: $"Item 2.3")
-                                )),
-                                new TreeViewComponent.Item(id: "3", label: $"Item 3")
-                            )
-                        ))
-                    ),
-                    F.View(
-                        style: new Style(
-                            backgroundColor: theme.DesignTokens[neutral].Background.Default,
-                            minHeight: new Length(100, LengthUnit.Percent),
-                            flexShrink: 1,
-                            flexGrow: 1,
-                            minWidth: new Length(100, LengthUnit.Pixel)
-                        ),
-                        children: F.Children(
-                            F.Text(
-                                style: new Style(
-                                    color: theme.DesignTokens[neutral].Text.Default,
-                                    fontSize: 36,
-                                    paddingLeft: 18,
-                                    paddingTop: 18,
-                                    paddingRight: 18,
-                                    paddingBottom: 18
-                                ),
-                                text: "Content"
-                            )
-                        )
-                    )
-                )
-            );
-        }
-    }
-
     void OnEnable()
     {
         if (_fiber == null)
@@ -119,7 +22,7 @@ public class DocsFiberRoot : MonoBehaviour
         }
         if (!_fiber.IsMounted)
         {
-            _fiber.Render(new DocsAppComponent());
+            _fiber.Render(new DocsRouting.RouterProvider());
         }
     }
 
